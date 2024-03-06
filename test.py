40203a59694f239a265d537bcb35b7817c5492d1
Eduardo A. Fuentes <ed.fue.01@gmail.com>
2024-03-06 06:59:24 -0700

    Function header

--- test.py
+++ test.py
@@ -3,3 +3,21 @@ print("test")
 a: int = 1
 b: int = 2
 c: int = a + b
+
+def missing_colon()
+    pass
+
+deff function_def_mispelled():
+    pass
+
+defi another_mispelled_function():
+    pass
+
+dfe yet_another_one():
+    pass
+
+df this_one_too():
+    pass
+
+def missing_parentheses
+    pass
