a1937667692cecd04bb4d1dac504c2a68dbeb619
CamiJolly1 <99227356+CamiJolly1@users.noreply.github.com>
2024-03-06 11:46:20 -0700

    Update indent.py

--- indent.py
+++ indent.py
@@ -21,8 +21,6 @@ def fix_indent(input_file):
             output_file.append(' ' * current_indent + line)
             if line.endswith(')') or line.endswith(']'):
                 current_indent -= 4
-            if current_indent <= 0:
-                current_indent == 0 # Indentation doesn't go negative 
     # joins the lines
     return '\n'.join(output_file)
 
